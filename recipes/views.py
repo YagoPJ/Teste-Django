b0c3a2c5c8f4be5f875b689ee7fea934661605d1
Kaio <projetosyago@gmail.com>
2025-08-31 21:41:39 -0300

    base-templates create

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 def sobre(request):
     return HttpResponse('SOBRE')
 def contato(request):
